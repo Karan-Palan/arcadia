29fd31dc08f78b96f0419ab51bc8362d77e4443b
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-01-25 19:53:05 +0530

    add: wallet integration logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,25 @@
 import Navbar from "./components/navbar";
 import LandingPage from "./components/landingPage";
+import Footer from "./components/footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { WalletProvider } from "./components/walletProvider";
 
 function App() {
   return (
+    <WalletProvider>
     <div className="press2start-regular">
+    {/* <div> */}
+      <Navbar />
       <BrowserRouter>
-        <Navbar />
+        
         <Routes>
           <Route path="/" element={<LandingPage/>} />
         </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
+    </WalletProvider>
+
   );
 }
 
